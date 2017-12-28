ad572908e7d2640a772f4674ec4943e426e9da23
przemekkot <przemyslaw.kot@gmail.com>
2017-12-28 12:55:30 +0100

    defined more csv data files

--- AppWidget/KodWindow.py
+++ AppWidget/KodWindow.py
@@ -7,6 +7,9 @@ from kivy.uix.label import Label
 
 from AppLibraries.Codes import Codes
 
+FRUIT_CODES_FILE = "Data/kody_produktow_owoce.csv"
+VEGETABLES_CODES_FILE = "Data/kody_produktow_warzywa.csv"
+BREAD_CODES_FILE = "Data/kody_produktow_pieczywo.csv"
 
 class KodWindow(GridLayout):
     padding = VariableListProperty(100)
@@ -16,8 +19,12 @@ class KodWindow(GridLayout):
     code_label = Label(text="123")
     next_code_button = Button(text="Następny kod")
 
+    codes_for_fruits = Codes(FRUIT_CODES_FILE)
+
     def __init__(self, **kwargs):
         super(KodWindow, self).__init__(**kwargs)
+
+
         self.add_widget(self.name_label)
         self.add_widget(self.code_label)
         self.add_widget(self.next_code_button)
@@ -25,7 +32,6 @@ class KodWindow(GridLayout):
         self.next_code_button.bind(on_press=self.choose_next_code)
 
     def choose_next_code(self, button_instance):
-            codes_for_fruits = Codes("Data/kody_produktow_owoce.csv")
-            code = codes_for_fruits.pick_one()
+            code = self.codes_for_fruits.pick_one()
             self.name_label.text = code[0]
             self.code_label.text = code[2]
