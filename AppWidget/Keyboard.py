a7a94b6b706632b9186b0c679970ac2b6c7cea09
przemekkot <przemyslaw.kot@gmail.com>
2017-12-29 13:03:34 +0100

    reseting code if wrong key pressed

--- AppWidget/Keyboard.py
+++ AppWidget/Keyboard.py
@@ -8,6 +8,7 @@ from AppWidget.KeyboardButton import KeyboardButton
 
 
 class Keyboard(GridLayout):
+    size_hint = (.5, .8)
     numbers = ["7","8","9","4","5","6","1","2","3", "0", "00"]
     keyboard_buttons = {}
     code_presenter = CodePresenter()
@@ -38,6 +39,7 @@ class Keyboard(GridLayout):
     def show_code(self, code_string):
         self.reset_buttons()
         self.code_label.clear_text()
+        self.code_label.reset_background()
         self.code_presenter = CodePresenter(code_string)
         self.highlight_next_button()
 
@@ -47,19 +49,30 @@ class Keyboard(GridLayout):
         self.keyboard_buttons[number].highlight()
 
     def show_next_button(self, button):
+        if not button.to_be_pressed:
+            self.show_code(self.code_presenter.code)
+            return
+
         button.show_green()
         self.code_label.text = self.code_label.text + button.text
         try:
             self.highlight_next_button()
         except IndexError:
-            pass
+            if self.code_presenter.is_code_right(self.code_label.text):
+                self.code_label.is_right()
+            else:
+                self.code_label.is_wrong()
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        number = int(keycode[1][-1])
-        if 10 > number >= 0 :
-            print(number)
-            self.keyboard_buttons[str(number)].dispatch("on_press")
+        if keycode[1] == "enter":
+            self.parent.choose_next_code()
+        try:
+            number = int(keycode[1][-1])
+            if 10 > number >= 0 :
+                self.keyboard_buttons[str(number)].dispatch("on_press")
+        except ValueError:
+            pass
