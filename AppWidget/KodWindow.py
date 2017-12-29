461801088475774a0c6caae40071912ee4aa23e3
przemekkot <przemyslaw.kot@gmail.com>
2017-12-29 12:46:31 +0100

    changed order of the widgets

--- AppWidget/KodWindow.py
+++ AppWidget/KodWindow.py
@@ -3,7 +3,6 @@
 from kivy.properties import VariableListProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 
 from AppLibraries.Codes import Codes
 from AppWidget.CodeLabel import CodeLabel
@@ -21,21 +20,20 @@ EMPTY_CODE = [_no_code_name, _empty_string, _no_code]
 class KodWindow(GridLayout):
     padding = VariableListProperty(100)
     spacing = VariableListProperty(10, length=2)
-
-    name_label = CodeNameLabel(text=_choose_code, font_size=40, size_hint=(.5,.5))
-    code_label = CodeLabel(text=_empty_string, bold=True, font_size=84)
-
-    keyboard = Keyboard(cols=1, rows=1, code_label=code_label, size_hint=(.5,.5))
-    next_code_button = Button(text=_next_code, size_hint=(.3, .3))
-
     codes = Codes()
+
+
+    name_label = CodeNameLabel(text=_choose_code)
+    code_label = CodeLabel(text=_empty_string)
+    keyboard = Keyboard(cols=1, rows=1, code_label=code_label)
+    next_code_button = Button(text=_next_code, size_hint=(.3, .3))
 
     def __init__(self, **kwargs):
         super(KodWindow, self).__init__(**kwargs)
 
-        self.add_widget(self.name_label)
         self.add_widget(self.code_label)
         self.add_widget(self.keyboard)
+        self.add_widget(self.name_label)
         self.add_widget(self.next_code_button)
 
         self.next_code_button.bind(on_press=self.choose_next_code_callaback)
