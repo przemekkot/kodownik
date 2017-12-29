ae462c42c644fe0e01b1f57147e9ce0f2dcc7125
przemekkot <przemyslaw.kot@gmail.com>
2017-12-29 12:47:29 +0100

    added reseting code added checking if good code was entered

--- AppLibraries/CodePresenter.py
+++ AppLibraries/CodePresenter.py
@@ -8,9 +8,18 @@ class CodePresenter:
 
     def __init__(self, code=""):
         self.code = code
+        self.make_signs(code)
+        self.empty = False
+
+    def make_signs(self, code):
         self.code_signs = list(code)
         self.code_signs.reverse()
-        self.empty = False
 
     def next_sign(self):
         return self.code_signs.pop()
+
+    def is_code_right(self, entered_code):
+        return self.code == entered_code
+
+    def reset_code(self):
+        self.make_signs(self.code)
