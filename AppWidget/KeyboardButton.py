0b102d0f7f542b043690734e56673874de7cd7c6
przemekkot <przemyslaw.kot@gmail.com>
2017-12-29 12:50:37 +0100

    added value of button to be pressed

--- AppWidget/KeyboardButton.py
+++ AppWidget/KeyboardButton.py
@@ -7,15 +7,18 @@ class KeyboardButton(Button):
     def __init__(self, **kwargs):
         super(KeyboardButton, self).__init__(**kwargs)
 
+        self.to_be_pressed = False
         self.background_color = [.1, .1, .1, 1]
         self.font_size = 30
         self.bold = True
 
     def reset_background(self):
+        self.to_be_pressed = False
         self.background_color = [.1, .1, .1, 1]
 
     def show_green(self):
         self.background_color = [0, .1, 0, 1]
 
     def highlight(self):
+        self.to_be_pressed = True
         self.background_color = [1, 0, 0, 1]
